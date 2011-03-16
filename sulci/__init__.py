5348f7d88c4c1b2c574a7319c1c47cb87480610f
Yohan Boniface <y.boniface@liberation.fr>
2011-03-16 22:40:19 +0100

    Catch ImportError in __init__.py, because setup.py import sulci, and Django isn't configured when setup.py is called. Is it the cleanest way ?

--- sulci/__init__.py
+++ sulci/__init__.py
@@ -1,5 +1,3 @@
-from django.db.models import loading
-from django.conf import settings
 
 __version_info__ = {
     'major': 0,
@@ -27,14 +25,18 @@ __version__ = get_version()
 #        return None # Fail silently for now.
 
 #descriptor_model = get_model_for_descriptor()
-def get_model_for_content():
-    return loading.get_model(settings.SULCI_CLI_CONTENT_APP_NAME, settings.SULCI_CLI_CONTENT_MODEL_NAME)
-content_model = get_model_for_content()
+try:
+    from django.db.models import loading
+    from django.conf import settings
+    def get_model_for_content():
+        return loading.get_model(settings.SULCI_CLI_CONTENT_APP_NAME, settings.SULCI_CLI_CONTENT_MODEL_NAME)
+    content_model = get_model_for_content()
 
-def get_manager_for_content():
-    name = getattr(settings, 'SULCI_CLI_CONTENT_MANAGER_NAME', 'objects')
-    manager = getattr(content_model, name)
-    return manager
-content_manager = get_manager_for_content()
-
+    def get_manager_for_content():
+        name = getattr(settings, 'SULCI_CLI_CONTENT_MANAGER_NAME', 'objects')
+        manager = getattr(content_model, name)
+        return manager
+    content_manager = get_manager_for_content()
+except ImportError: # setup.py import sulci...
+    print "Warning. It seems that Django isn't configured properly."
 
