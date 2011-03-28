3bee41bdad2a8e6ca44af3563fc62ef3bd516f09
Yohan Boniface <y.boniface@liberation.fr>
2011-03-28 14:28:17 +0200

    Make settings optionnal, if don't want to use command line or make a training

--- sulci/__init__.py
+++ sulci/__init__.py
@@ -28,16 +28,24 @@ __version__ = get_version()
 try:
     from django.db.models import loading
     from django.conf import settings
-    def get_model_for_content():
-        return loading.get_model(settings.SULCI_CLI_CONTENT_APP_NAME,
-                                 settings.SULCI_CLI_CONTENT_MODEL_NAME)
-    content_model = get_model_for_content()
-
-    def get_manager_for_content():
-        name = getattr(settings, 'SULCI_CLI_CONTENT_MANAGER_NAME', 'objects')
-        manager = getattr(content_model, name)
-        return manager
-    content_manager = get_manager_for_content()
 except ImportError: # setup.py import sulci...
     print "Warning. It seems that Django isn't configured properly."
+else:
+    try:
+        def get_model_for_content():
+            return loading.get_model(settings.SULCI_CLI_CONTENT_APP_NAME,
+                                     settings.SULCI_CLI_CONTENT_MODEL_NAME)
+        content_model = get_model_for_content()
+
+        def get_manager_for_content():
+            name = getattr(settings, 'SULCI_CLI_CONTENT_MANAGER_NAME', 'objects')
+            manager = getattr(content_model, name)
+            return manager
+        content_manager = get_manager_for_content()
+    except AttributeError:
+        # No settings where setted (possible, is command line and training are no
+        # planed.
+        print "Missing settings : command line and training will no be usable."
+        content_model = None
+        content_manager = None
 
