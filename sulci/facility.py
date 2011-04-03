52610c14fa064d310785271fcb541716c84c94b2
Yohan Boniface <y.boniface@liberation.fr>
2011-04-04 00:32:25 +0200

    Add security checks in merge_descriptors facility

--- sulci/facility.py
+++ sulci/facility.py
@@ -5,12 +5,18 @@ This module provide some facilities who can be used outside sulci.
 
 from sulci.log import sulci_logger
 
-def merge_descriptors(origin, destination):
+def merge_descriptors(origin, destination, force=False):
     """
     This facility take all relations of a descriptor, add or create the weigth
     of these relations to the destination descriptor relations, and delete
     the origin descriptors relations.
     """
+    if origin == destination:
+        raise ValueError("Origin and destination can't be equal !")
+    origin_count = origin.triggertodescriptor_set.count()
+    destination_count = destination.triggertodescriptor_set.count()
+    if origin_count > destination_count and not force:
+        raise ValueError("Origin has more relations than destination, use force.")
     # We loop over the origin relations
     for relation in origin.triggertodescriptor_set.all():
         sulci_logger.info(u"Handle relation %s" % unicode(relation))
