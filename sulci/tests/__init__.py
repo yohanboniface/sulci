1c84734dfaf979f8e8585ea27e74468a595c281e
Yohan Boniface <y.boniface@liberation.fr>
2012-01-19 18:37:03 +0100

    Add is_strong_punctuation tests

--- sulci/tests/__init__.py
+++ sulci/tests/__init__.py
@@ -1,1 +1,2 @@
 from sulci.tests.textutils import *
+from sulci.tests.token import *
--- sulci/tests/token.py
+++ sulci/tests/token.py
@@ -7,10 +7,28 @@ from django.test import TestCase
 
 from sulci.base import Token
 
-__all__ = ["TokenTest"]
+__all__ = ["TokenTest", "TokenIsStrongPunctuationTest"]
 
 class TokenTest(TestCase):
-
+    
     def test_should_create_a_token(self):
         token = Token("mot", original="mot")
         self.assertTrue(isinstance(token, Token))
+
+class TokenIsStrongPunctuationTest(TestCase):
+    
+    def test_period_is_strong_punctuation(self):
+        token = Token("xxx", ".")
+        self.assertTrue(token.is_strong_punctuation())
+    
+    def test_question_mark_is_strong_punctuation(self):
+        token = Token("xxx", "?")
+        self.assertTrue(token.is_strong_punctuation())
+    
+    def test_exclamation_point_is_strong_punctuation(self):
+        token = Token("xxx", "!")
+        self.assertTrue(token.is_strong_punctuation())
+    
+    def test_comma_is_not_strong_punctuation(self):
+        token = Token("xxx", ",")
+        self.assertFalse(token.is_strong_punctuation())
