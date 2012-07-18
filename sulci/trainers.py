09bb7d0372a027a9370ea2c87b812a90060b71f5
Yohan Boniface <y.boniface@liberation.fr>
2012-07-18 22:36:49 +0200

    pep8 on trainers.py

--- sulci/trainers.py
+++ sulci/trainers.py
@@ -4,14 +4,13 @@ import os
 import time
 import datetime
 
-from sulci.utils import load_file, save_to_file
 from sulci.thesaurus import Trigger, Descriptor
 from sulci.textmining import SemanticalTagger
-from sulci.textutils import tokenize_text
 from sulci.rules_templates import LemmatizerTemplateGenerator, RuleTemplate,\
                            ContextualTemplateGenerator, LexicalTemplateGenerator
 from sulci.log import sulci_logger
 from sulci import config
+
 
 class SemanticalTrainer(object):
     """
@@ -19,12 +18,12 @@ class SemanticalTrainer(object):
     """
     PENDING_EXT = ".pdg"
     VALID_EXT = ".trg"
-    
+
     def __init__(self, thesaurus, pos_tagger, mode="full"):
         self.thesaurus = thesaurus
         self.pos_tagger = pos_tagger
         self.mode = mode
-    
+
     def begin(self):
         """
         Make one trigger for each descriptor of the thesaurus.
@@ -34,7 +33,7 @@ class SemanticalTrainer(object):
         for d in self.thesaurus:
             t = Trigger.get_or_connect(original=unicode(d))
             t.connect(d, 1)
-    
+
     def setup_socket_master(self):
         """
         Configure the sockets for the master trainer.
@@ -49,7 +48,7 @@ class SemanticalTrainer(object):
         # No response is expected.
         self.pubsocket = zmq.Socket(zmq.Context(), zmq.PUB)
         self.pubsocket.bind("ipc:///tmp/sulci.apply")
-    
+
     def setup_socket_slave(self):
         """
         Configure sockets for the workers (slaves).
@@ -68,7 +67,7 @@ class SemanticalTrainer(object):
 #        self.reppoller = zmq.Poller()
 #        self.reppoller.register(self.repsocket, zmq.POLLIN)
         self.subsocket.setsockopt(zmq.SUBSCRIBE, "")
-    
+
     def do(self, *args):
         if self.mode == "slave":
             self.slave()
@@ -84,7 +83,7 @@ class SemanticalTrainer(object):
             step = 1000
             forloop_remaining = total = len(qs)
             for loop in range(len(qs) / step + 1):
-                _from = loop*step
+                _from = loop * step
                 _to = _from + step
                 current_qs = qs[_from:_to]
                 print "MASTER -- Processing qs from %d to %d" % (_from, _to)
@@ -105,14 +104,14 @@ class SemanticalTrainer(object):
                         t_diff = t_now - t_init
                         average = t_diff / (total - forloop_remaining)
                         time_remaining = average * forloop_remaining
-                        ETA = datetime.datetime.now() + datetime.timedelta(0,time_remaining)
+                        ETA = datetime.datetime.now() + datetime.timedelta(0, time_remaining)
                         # Using print, as I launch this huge script
                         # with python -O (so not __debug__, so no log)
                         print "MASTER -- %s -- %s remaining to process -- Avg: %s s -- ETA : %s" %\
                                (status, forloop_remaining, round(average, 2), ETA.strftime("%a %d %R"))
             if self.mode == "master":
                 self.pubsocket.send(" stop")
-    
+
     def slave(self):
         self.setup_socket_slave()
         print "SLAVE %s -- ready" % os.getpid()
@@ -122,7 +121,8 @@ class SemanticalTrainer(object):
                 # Used just to stop the process
                 # All the subprocess receive the same socket
                 action = self.subsocket.recv()[1:]
-                if action == "stop": return
+                if action == "stop":
+                    return
             # This is the REP mode
             # We receive an action to do, with a pk
             idx, pk = self.repsocket.recv_multipart()
@@ -133,7 +133,7 @@ class SemanticalTrainer(object):
             self.repsocket.send_multipart(
                 [idx, "Processed pk %s (%s s)" % (pk, round(tot_time, 2))]
             )
-    
+
     def train(self, inst):
         """
         For the moment, human defined descriptors are a string with "," separator.
@@ -149,7 +149,8 @@ class SemanticalTrainer(object):
         validated_descriptors = set()
         # Retrieve descriptors
         for d in descriptors:
-            if not d: continue
+            if not d:
+                continue
             # d = d.strip().replace(u"’", u"'")
             # We create the descriptor not in thesaurus for now
             # because descriptors in article and thesaurus are not
@@ -164,12 +165,12 @@ class SemanticalTrainer(object):
         # Retrieve keytentities :
         try:
             S = SemanticalTagger(
-                text, 
-                thesaurus=self.thesaurus, 
-                pos_tagger=self.pos_tagger, 
+                text,
+                thesaurus=self.thesaurus,
+                pos_tagger=self.pos_tagger,
                 lexicon=self.pos_tagger.lexicon
             )
-            S.deduplicate_keyentities() # During lairning, try to filter
+            S.deduplicate_keyentities()  # During lairning, try to filter
         except ValueError:
             # SemanticalTagger raise ValueError if text is empty
             return
@@ -199,24 +200,24 @@ class SemanticalTrainer(object):
 #        false_positive = calculated_descriptors.difference(validated_descriptors)
 #        #Validated descriptors that where also calculated
 #        true_positive = calculated_descriptors.intersection(validated_descriptors)
-#        
+#
 #        for d in true_positive:
 #            for t in current_triggers:
 #                if d in t:
 #                    t.connect(d, t.current_score)#trust the relation
 #                    log(u"Adding 2 to connection %s - %s" % (t, d), "YELLOW")
-#        
+#
 #        for d in false_positive:
 #            for t in current_triggers:
 #                if d in t:
 #                    t.connect(d, -t.current_score)#untrust the relation
 #                    log(u"Removing 1 to connection %s - %s" % (t, d), "BLUE")
-#        
+#
 #        for d in false_negative:
 #            for t in current_triggers:
 #                t.connect(d, t.current_score)#guess the relation
 #                log(u"Connecting %s and %s" % (t, d), "WHITE")
-    
+
     def clean_connections(self):
         """
         Delete all the connection where score < 0.
@@ -224,11 +225,12 @@ class SemanticalTrainer(object):
         # We maybe have to clean connections where pondered_weigth is < 0.1.
         Trigger.clean_all_connections()
 
+
 class RuleTrainer(object):
     """
     Main trainer class for rules based, for factorisation.
     """
-    
+
     def do(self):
         if self.mode == "slave":
             self.slave()
@@ -238,7 +240,7 @@ class RuleTrainer(object):
             self.pubsocket.send(" stop")
         else:
             self.train()
-    
+
     def setup_socket_master(self):
         """
         Configure the sockets for the master trainer.
@@ -252,7 +254,7 @@ class RuleTrainer(object):
         # No response is expected.
         self.pubsocket = zmq.Socket(zmq.Context(), zmq.PUB)
         self.pubsocket.bind("ipc:///tmp/sulci.apply")
-    
+
     def setup_socket_slave(self):
         """
         Configure sockets for the workers (slaves).
@@ -269,13 +271,14 @@ class RuleTrainer(object):
         self.reppoller = zmq.Poller()
         self.reppoller.register(self.repsocket, zmq.POLLIN)
         self.subsocket.setsockopt(zmq.SUBSCRIBE, "")
-    
+
     def slave(self):
         self.setup_socket_slave()
         while True:
             if self.subpoller.poll(0):
                 rule = self.subsocket.recv()[1:]
-                if rule == "stop": return
+                if rule == "stop":
+                    return
                 rule = rule.decode("utf-8")
                 template = self.get_template_instance(rule)
                 #Apply the rule to the tokens
@@ -285,7 +288,7 @@ class RuleTrainer(object):
                 idx, action, rule = self.repsocket.recv_multipart()
                 _, good, bad = self.test_rule(rule.decode("utf-8"))
                 self.repsocket.send_multipart([idx, rule, str(good), str(bad)])
-    
+
     def test_rules(self, rules_candidates):
         pondered_rules = []
         if self.mode == "master":
@@ -303,22 +306,22 @@ class RuleTrainer(object):
             for rule in rules_candidates:
                 pondered_rules.append(self.test_rule(rule))
         return pondered_rules
-    
+
     def pretrain(self):
         """
         Trainer specific training session preparation.
         """
         pass
-    
+
     def log_error(self, token):
         pass
-    
+
     def get_template_instance(self, tpl):
         pass
-    
+
     def select_one_rule(self, rules):
         pass
-    
+
     def train(self):
         """
         Main factorized train method.
@@ -343,7 +346,8 @@ class RuleTrainer(object):
                 # Select one rule
                 rule_candidate, score = self.select_one_rule(pondered_rules)
                 # Maybe the test "rule_candidate in final_rules" have to be done before...
-                if rule_candidate and not rule_candidate in final_rules:#How to calculate the score min ?
+                if rule_candidate and not rule_candidate in final_rules:
+                    # How to calculate the score min ?
                     template = self.get_template_instance(rule_candidate)
                     final_rules.append((rule_candidate, score))
                     # Apply the rule to the tokens
@@ -355,16 +359,17 @@ class RuleTrainer(object):
                     run_applied_rule = True
                     # We have applied a rule, we can try another run
                     errors = self.get_errors()
-                    break#break the for
-                else:#No rule applied for this error
+                    break  # break the for
+                else:  # No rule applied for this error
                     # We don't want to reprocess this error another time
                     # unless the sample (so the context) as changed.
                     token_with_error.sample.set_trained_position(token_with_error.position)
-            if run_applied_rule: continue#go back to while
-            errors = None#Nothing applied, we stop here.
+            if run_applied_rule:
+                continue  # go back to while
+            errors = None  # Nothing applied, we stop here.
         self.display_errors()
         self.template_generator.export(final_rules)
-    
+
     def test_rule(self, rule):
         template = self.get_template_instance(rule)
         bad = 0
@@ -377,7 +382,7 @@ class RuleTrainer(object):
                 bad += 1
         sulci_logger.info(u"%s g: %d b : %d" % (rule, good, bad), "GRAY")
         return rule, good, bad
-    
+
     def display_errors(self):
         """
         Display errors in current step.
@@ -389,7 +394,7 @@ class RuleTrainer(object):
            (errors_count, 100.0 * errors_count / total_words, total_words), "RED")
         for r_error in remaining_errors:
             self.log_error(r_error)
-    
+
     def get_errors(self):
         """
         Retrieve token where tag !== verified_tag.
@@ -400,35 +405,36 @@ class RuleTrainer(object):
             final += sample.get_errors(self.attr_name)
         return final
 
+
 class LemmatizerTrainer(RuleTrainer):
     """
     Train the Lemmatizer.
     """
     template_generator = LemmatizerTemplateGenerator
-    attr_name = "lemme" # Name of the attribute we test on 
+    attr_name = "lemme"  # Name of the attribute we test on
 
-    def __init__(self, lemmatizer, mode = "full"):
+    def __init__(self, lemmatizer, mode="full"):
         self.lemmatizer = lemmatizer
         self.mode = mode
         self.tokens = self.lemmatizer.tokens
         self.samples = self.lemmatizer.samples
         self.pretrain()
-    
+
     def pretrain(self):
         """
         We need to have the right tags, here
         """
         for token in self.tokens:
             token.tag = token.verified_tag
-    
+
     def log_error(self, token):
         sulci_logger.info(u"Error : %s, lemmatized %s instead of %s" \
             % (unicode(token), token.lemme, token.verified_lemme), "WHITE")
-    
+
     def get_template_instance(self, tpl):
         template, _ = self.template_generator.get_instance(tpl)
         return template
-    
+
     def select_one_rule(self, rules):
         """
         Having a set of rules candidate for correcting some error, select the
@@ -436,44 +442,46 @@ class LemmatizerTrainer(RuleTrainer):
         """
         return RuleTemplate.select_one(rules, len(self.tokens))
 
+
 class POSTrainer(RuleTrainer):
     """
     Pos Tagger trainer.
     """
-    attr_name = "tag" # Name of the attribute we test on 
-    
-    def __init__(self, tagger, corpus, mode = "full"):
+    attr_name = "tag"  # Name of the attribute we test on
+
+    def __init__(self, tagger, corpus, mode="full"):
         self.tagger = tagger
         self.corpus = corpus
         self.pretrain()
         self.tokens = self.corpus.tokens
         self.samples = self.corpus.samples
         self.mode = mode
-    
+
     def log_error(self, token):
         sulci_logger.info(u"Error : %s, tagged %s instead of %s" \
                      % (unicode(token), token.tag, token.verified_tag), "WHITE")
-    
+
     def get_template_instance(self, tpl):
         template, _ = self.template_generator.get_instance(tpl, lexicon=self.tagger.lexicon)
         return template
-    
+
     def select_one_rule(self, rules):
         return RuleTemplate.select_one(rules, len(self.corpus), 3)
-    
+
     def pretrain(self):
         self.tagger.tag_all(self.corpus.tokens)
 
+
 class LexicalTrainer(POSTrainer):
-    
+
     template_generator = LexicalTemplateGenerator
-    
+
     def pretrain(self):
         """
         Tag the tokens, but not using POS rules, as we are training it.
         """
         self.tagger.tag_all(self.corpus.tokens, lexical=False, contextual=False)
-    
+
     def get_errors(self):
         """
         We don't care about token in Lexicon, for lexical trainer.
@@ -484,13 +492,13 @@ class LexicalTrainer(POSTrainer):
             final += [t for t in sample.get_errors() if not t in self.tagger.lexicon]
         return final
 
+
 class ContextualTrainer(POSTrainer):
-    
+
     template_generator = ContextualTemplateGenerator
-    
+
     def pretrain(self):
         """
         Tag the tokens, but not using the contextual rules, as we are training it.
         """
         self.tagger.tag_all(self.corpus.tokens, contextual=False)
-
