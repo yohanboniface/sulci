f4c9eb20eea0986c6a288c42f754cc26d9e13233
Yohan Boniface <yb@enix.org>
2012-09-13 23:54:31 +0200

    pep8 on lexicon.py

--- sulci/lexicon.py
+++ sulci/lexicon.py
@@ -11,16 +11,17 @@ For now, the lexicon is stored in a flat file, with special syntax :
 from collections import defaultdict
 from operator import itemgetter
 
-from base import TextManager, RetrievableObject
+from base import TextManager
 from utils import load_file, save_to_file
 from sulci.log import sulci_logger
 from corpus import Corpus
+
 
 class Lexicon(TextManager):
     """
     The lexicon is a list of unique words and theirs possible POS tags.
     """
-    
+
     def __init__(self, path="corpus"):
         self.CORPUS_EXT = ".lxc.lem.crp"
         self.VALID_EXT = ".lxc"
@@ -31,32 +32,32 @@ class Lexicon(TextManager):
         self._prefixes = None
         self._suffixes = None
         self.factors = set()
-    
+
     def __iter__(self):
         return self.loaded.__iter__()
-    
+
     def __getitem__(self, item):
         return self.loaded.__getitem__(item)
-    
+
     def __len__(self):
         return len(self.loaded)
-    
+
     def items(self):
         return self.loaded.items()
-    
+
     def __contains__(self, key):
         if isinstance(key, object) and key.__class__.__name__ == "Token":
             key = key.original
         return key in self.loaded
-    
+
     @property
     def loaded(self):
         """
         Load lexicon in RAM, from file.
-        
+
         The representation will be a dict {"word1": [{tag1 : lemme1}]}
         """
-        if self._loaded is None: # Caching and lazy loading
+        if self._loaded is None:  # Caching and lazy loading
             sulci_logger.debug("Loading lexicon...", "RED", True)
             lx = load_file("%s/lexicon.lxc" % self.PATH)
             self._loaded = {}
@@ -66,11 +67,11 @@ class Lexicon(TextManager):
                     self.add_factors(lexicon_entity.word)
                     self._loaded[lexicon_entity.word] = lexicon_entity
         return self._loaded
-    
+
     def add_factors(self, token):
         """
         Build the list of factors (pieces of word).
-        
+
         These factors are used by the POStagger, to determine if an
         unnown word could be a derivate of another.
         """
@@ -78,11 +79,11 @@ class Lexicon(TextManager):
         while prefix:
             suffix = prefix
             while suffix:
-                if not suffix == token:#Don't add the initial graph
+                if not suffix == token:  # Don't add the initial graph
                     self.factors.add(suffix)
                 suffix = suffix[1:]
             prefix = prefix[:-1]
-    
+
     def make(self, force=False):
         """
         Build the lexicon.
@@ -105,21 +106,21 @@ class Lexicon(TextManager):
                 lemme_to_original[tk.original][tk.verified_tag] = defaultdict(int)
             # Frequence of this lemme for this tag for this word...
             lemme_to_original[tk.original][tk.verified_tag][tk.verified_lemme] += 1
-        
+
         def get_one_line(key):
             """
             Return one line of the lexicon.
             Take the token.original string in parameter.
             """
             return u"%s\t%s" % (key, get_tags(key))
-        
+
         def get_tags(key):
             """
             Return sorted tags for a original word compiled in a string :
             tag/lemme tag/lemme
             """
             # Retrieve tags
-            tags = sorted([(k, v) for k, v in final[key].iteritems()], 
+            tags = sorted([(k, v) for k, v in final[key].iteritems()],
                                              key=itemgetter(1), reverse=True)
             # Build final datas
             final_data = []
@@ -127,17 +128,17 @@ class Lexicon(TextManager):
                 computed_lemmes = get_lemmes(key, tag)
                 lemme, score = computed_lemmes[0]
                 final_data.append(u"%s/%s" % (tag, lemme))
-            
+
             # Return it as a string
             return u" ".join(final_data)
-        
+
         def get_lemmes(key, tag):
             """
             Return sorted lemmes for one word with one POS tag.
             """
-            return sorted(((k, v) for k, v in lemme_to_original[key][tag].iteritems()), 
+            return sorted(((k, v) for k, v in lemme_to_original[key][tag].iteritems()),
                                                 key=itemgetter(1), reverse=True)
-        
+
         d = []
         for k, v in sorted(final.iteritems()):
             d.append(get_one_line(k))
@@ -145,7 +146,7 @@ class Lexicon(TextManager):
 #            d +=  u"%s\t%s\n" % (k, " ".join([u"%s/%s" % (tp[0], sorted(lemme_to_original[k][tp[0]], key=itemgetter(1), reverse=True)[0]) for tp in sorted([(k2, v2) for k2, v2 in v.iteritems()], key=itemgetter(1), reverse=True)]))
         ext = force and self.VALID_EXT or self.PENDING_EXT
         save_to_file("%s/lexicon%s" % (self.PATH, ext), unicode(final_d))
-    
+
     def create_afixes(self):
         """
         We determinate here the most frequent prefixes and suffixes.
@@ -164,34 +165,34 @@ class Lexicon(TextManager):
                 suffixes[suffix] += len(suffix)
         #We make a set, to speed contains, so sorted doesn't meens nothing
         self._prefixes = set(key for key, value in \
-                         sorted(((k, v) for k, v in prefixes.items() if v > len(k) * 2), 
+                         sorted(((k, v) for k, v in prefixes.items() if v > len(k) * 2),
                          key=itemgetter(1), reverse=True))
         self._suffixes = set(key for key, value in \
-                         sorted(((k, v) for k, v in suffixes.items() if v > len(k) * 2), 
+                         sorted(((k, v) for k, v in suffixes.items() if v > len(k) * 2),
                          key=itemgetter(1), reverse=True))
-    
+
     @property
     def prefixes(self):
         if self._prefixes is None:
             self.create_afixes()
         return self._prefixes
-    
+
     @property
     def suffixes(self):
         if self._suffixes is None:
             self.create_afixes()
         return self._suffixes
-    
+
     def get_entry(self, entry):
         if entry in self:
             sulci_logger.info(unicode(self[entry]), "WHITE")
         else:
             sulci_logger.info(u'No entry for "%s"' % entry, "WHITE")
-    
+
     def check(self):
         """
         Util method to try to individuate errors in the Lexicon.
-        For this, we display the entries with several tags, in case 
+        For this, we display the entries with several tags, in case
         they are wrong duplicate.
         """
         for key, entity in self.items():
@@ -199,11 +200,12 @@ class Lexicon(TextManager):
                 sulci_logger.info(u"%s tags for %s" % (len(entity.tags), key), "RED")
                 sulci_logger.info(entity.tags, "WHITE")
 
+
 class LexiconEntity(object):
     """
     One word of a lexicon.
     """
-    
+
     def __init__(self, raw_data, **kwargs):
         self.default_tag = None
         self.default_lemme = None
@@ -218,12 +220,12 @@ class LexiconEntity(object):
             if self.default_lemme is None:
                 self.default_lemme = lemme
             self.tags[tag] = lemme
-    
+
     def __unicode__(self):
         return u"%s => %s" % (self.word, self.tags)
-    
+
     def __contains__(self, key):
         return self.tags.__contains__(key)
-    
+
     def __getitem__(self, key):
         return self.tags[key]
