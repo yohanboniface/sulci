aa073eaea333f5dc6bf8d066972edf32e8ff2a20
Yohan Boniface <yb@enix.org>
2012-09-27 20:53:17 +0200

    Filter git and hg requirements otherwise setuptools complains

--- setup.py
+++ setup.py
@@ -5,22 +5,22 @@ import sulci
 
 try:
     reqs = open(os.path.join(os.path.dirname(__file__),
-                             'requirements.txt')).read()
+                             'requirements.txt')).read().split()
+    reqs = [req for req in reqs if not req.startswith(('git+', 'hg+'))]
 except (IOError, OSError):
     reqs = ''
 
-
+print reqs
 setup(
     name='sulci',
     version=sulci.get_version(),
-    description=
-    'French textmining tool based on Liberation corpus and thesaurus',
+    description='French textmining tool based on Liberation corpus and thesaurus',
     author='Yohan Boniface',
     author_email='y.boniface@liberation.fr',
     url='https://github.com/yohanboniface/sulci',
     packages=find_packages(),
     include_package_data=True,
-    package_data={'sulci': ['corpus/*','templates/sulci/*']},
+    package_data={'sulci': ['corpus/*', 'templates/sulci/*']},
     install_requires=reqs,
     classifiers=[
         'Development Status :: 3 - Alpha',
